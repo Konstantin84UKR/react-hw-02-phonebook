8c49dd298dd69059153a483cb8e419f8efb35616
Konstantin84UKR <110284bkj@gmail.com>
2021-04-26 21:51:46 +0300

    fix_26042021

--- src/components/ContactList/ContactList .js
+++ src/components/ContactList/ContactList .js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import styles from "./contactList.module.css";
 
 export default class ContactList extends Component {
+
+ 
+  
   render() {
 
     const {contactsChange,filter,contactDelete} = this.props
-
+    let odd = true;
+  
     return (
       <div>
         <ul>
           {contactsChange.map(({id,name,number}, index) => {
           
             if (name.toUpperCase().includes(filter.toUpperCase())) {
+
+              odd = odd ? false : true
               return (
-             
-                  <li key={id} className={index % 2 === 0 ? styles.odd : styles.even}>
+              
+                  <li key={id} className={odd===true ? styles.odd : styles.even}>
                     {`${name} : ${number}`}
                    
                     <button  onClick={() => {
